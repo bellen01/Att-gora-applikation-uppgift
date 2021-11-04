bff442a6a116743a8edea6e95528de0a47f972fe
Isabelle <isabelle.annie.karlsson@gmail.com>
2021-11-04 22:34:00 +0100

    Done with remove all chores

--- js/main.js
+++ js/main.js
@@ -10,16 +10,27 @@ addChore.addEventListener('click', function () {
     newChoreInput.setAttribute('type', 'text');
     input.value = '';
     newChoreInput.setAttribute('disabled', 'true');
+    //newChoreInput.toggleAttribute("disabled");
+
 
     let changeChoreBtn = document.createElement('button');
     changeChoreBtn.innerHTML = "Ändra";
+    changeChoreBtn.addEventListener('click', function () {
+        changeChoreBtn.innerHTML = "Spara";
+        newChoreInput.toggleAttribute("disabled");
+    })
 
     let completedChoreBtn = document.createElement('button');
     completedChoreBtn.innerHTML = "Färdig";
+    completedChoreBtn.addEventListener('click', function () {
 
+    })
 
     let deleteChoreBtn = document.createElement('button');
     deleteChoreBtn.innerHTML = "Radera";
+    deleteChoreBtn.addEventListener('click', function (e) {
+        e.target.parentNode.remove();
+    })
 
     let toDoChores = document.getElementById('to-do-list');
     toDoChores.append(li);
@@ -27,3 +38,28 @@ addChore.addEventListener('click', function () {
 
 })
 
+// let removeAllChores = document.getElementById('remove-all-chores-btn');
+// removeAllChores.addEventListener('click', function () {
+//     let li = document.getElementsByTagName('li');
+//     li.innerHTML = "";
+// })
+
+//Återställ knappen
+let removeAllChores = document.getElementById('remove-all-chores-btn');
+removeAllChores.addEventListener('click', function () {
+    let ul = document.getElementsByTagName('ul');
+    for (let i of ul) {
+        while (i.lastElementChild) {
+            i.firstElementChild.remove();
+        }
+    }
+})
+
+// let removeAllChores = document.getElementById('remove-all-chores-btn');
+// removeAllChores.addEventListener('click', function () {
+//     for (let i of ul) {
+//         for (let child of i) {
+//             child.lastElementChild.remove();
+//         }
+//     }
+// })
